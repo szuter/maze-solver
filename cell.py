cae39a393b6a5a7f4f8d3926920119092f871445
szuter <piotrstrzelec1989@gmail.com>
2025-01-10 12:08:46 +0100

    Add Cell class for maze representation and update main to draw cells

---
+++ cell.py
@@ -0,0 +1,26 @@
+from graphics import Point, Line
+
+
+class Cell:
+    def __init__(self, window):
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+
+        self.has_left_wall = True
+        self.has_top_wall = True
+        self.has_right_wall = True
+        self.has_bottom_wall = True
+
+        self._window = window
+
+    def draw(self, x1, y1, x2, y2):
+        if self.has_left_wall:
+            self._window.draw_line(Line(Point(x1, y1), Point(x1, y2)))
+        if self.has_top_wall:
+            self._window.draw_line(Line(Point(x1, y1), Point(x2, y1)))
+        if self.has_right_wall:
+            self._window.draw_line(Line(Point(x2, y1), Point(x2, y2)))
+        if self.has_bottom_wall:
+            self._window.draw_line(Line(Point(x1, y2), Point(x2, y2)))
