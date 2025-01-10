0ee45d76ba3f20556be25d2bccb41e7377ffb590
szuter <piotrstrzelec1989@gmail.com>
2025-01-10 12:33:55 +0100

    Enhance Cell drawing functionality and implement draw_move method for visual transitions

--- cell.py
+++ cell.py
@@ -16,6 +16,10 @@ class Cell:
         self._window = window
 
     def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
         if self.has_left_wall:
             self._window.draw_line(Line(Point(x1, y1), Point(x1, y2)))
         if self.has_top_wall:
@@ -24,3 +28,14 @@ class Cell:
             self._window.draw_line(Line(Point(x2, y1), Point(x2, y2)))
         if self.has_bottom_wall:
             self._window.draw_line(Line(Point(x1, y2), Point(x2, y2)))
+
+    def draw_move(self, to_cell, undo=False):
+        self._window.draw_line(
+            Line(
+                Point((self._x1 + self._x2) // 2, (self._y1 + self._y2) // 2),
+                Point(
+                    (to_cell._x1 + to_cell._x2) // 2, (to_cell._y1 + to_cell._y2) // 2
+                ),
+            ),
+            "grey" if undo else "red",
+        )
